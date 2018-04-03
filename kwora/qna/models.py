d415343af06e728a0a070c6e2cc19911175d07da
Ravi Ojha <ojha.ravi@flipkart.com>
2018-04-03 13:48:43 +0530

    qna: Add new model class Question in qna

--- kwora/qna/models.py
+++ kwora/qna/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    author = models.ForeignKey(User)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
