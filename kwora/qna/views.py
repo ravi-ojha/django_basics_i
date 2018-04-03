a0852f0caf2fa0514bf4e7ffcb12ef92e7b75351
Ravi Ojha <ojha.ravi@flipkart.com>
2018-04-03 14:38:12 +0530

    qna: Add basic template syntax to render list of questions

--- kwora/qna/views.py
+++ kwora/qna/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Question
 
 # Create your views here.
 def question_list(request):
-    return render(request, 'qna/question_list.html', context={})
+    questions = Question.objects.all()
+    return render(request, 'qna/question_list.html', context={'questions': questions})
